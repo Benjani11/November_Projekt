ed428e839b028f3a9059747ba392e55ea5fade85
Benjani11 <60339652+Benjani11@users.noreply.github.com>
2020-12-03 15:44:31 +0100

    skapat projektet

---
+++ November Projekt/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace November_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
